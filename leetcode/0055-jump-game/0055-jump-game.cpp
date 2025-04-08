d249bd7914354a301e47123b5816249708393c14
Jisu Kim <hellojs242@gmail.com>
2025-04-08 16:53:06 +0900

    Time: 159 ms (15.83%), Space: 53.2 MB (16.16%) - LeetHub

--- leetcode/0055-jump-game/0055-jump-game.cpp
+++ leetcode/0055-jump-game/0055-jump-game.cpp
@@ -5,18 +5,18 @@ private:
     // reverse side brute force
     bool findNextHop(vector<int>& nums, int index) {
         // 현재 노드가 마지막 노드이거나 그보다 크면 끝에 도달한 것이다.
-        if (index >= nums.size() - 1) {
-            return true;
-        }
-        if (nums[index] == 0 || visited[index] == true) {
-            return false;
-        }
         visited[index] = true;
         int range = nums[index];
 
         // 현재 노드가 0이 아닐 때까지 진행할 수 있다.
         // true가 나올때까지 index + 1 ~ index + range를 역순으로 돈다.
         for (;range > 0; --range) {
+            if (index + range >= nums.size() - 1) {
+                return true;
+            }
+            if (nums[index + range] == 0 || visited[index + range] == true) {
+                continue;
+            }
             if(findNextHop(nums, index + range)) { // 이전 노드들이 다 true이면 true이다.
                 return true;
             }
@@ -27,6 +27,9 @@ private:
 
 public:
     bool canJump(vector<int>& nums) {
+        if (nums.size() <= 1) {
+            return true;
+        }
         return findNextHop(nums, 0);
     }
 };
