a233e0fb213859c8c2180eb26ea2cd2cb860087e
Jisu Kim <hellojs242@gmail.com>
2025-04-10 14:11:08 +0900

    Time: 0 ms (100%), Space: 11.3 MB (89.48%) - LeetHub

--- leetcode/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ leetcode/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -7,22 +7,18 @@ public:
         }
         int start = 0;
         int maxLen = 0;
-        int strLen = s.length();
-        for (int i = 0; i < strLen; ++i) {
-            if (alpha[s[i]] != -1) {
-                maxLen = max(maxLen, i - start); // 현재글자 이전까지의 길이를 저장
-                // abb -> b ab
-                // ba -> b
-                // cout << "start: " << start<< ", end: " << i << ", maxLen: " << maxLen<<endl;
-                // cout << s.substr(start, i + 1) << endl;
-                start = max(start, alpha[s[i]] + 1); // 앞글자를 제외
+
+        for (int i = 0; i < s.length(); ++i) {
+            // 문자가 이미 현재 부분 문자열 내에 있는 경우, 시작점을 이동
+            if (alpha[s[i]] >= start) {
+                start = alpha[s[i]] + 1;
             } 
+            // 현재 부분 문자열 길이 갱신
+            maxLen = max(maxLen, i - start + 1);
+            
+            // 현재 문자의 위치 갱신
             alpha[s[i]] = i;
         }
-        // start가 움직이지 않았을 경우, 겹친 숫자가 나오지 않았을 경우 start는 0이다.
-        cout << "start: " << start<< ", end: " << strLen << ", maxLen: " << maxLen<<endl;
-
-        maxLen = max(maxLen, strLen - start);
 
         return maxLen;
     }
