dbfb36b6570c0bc6b0c6b8830859110ac7698932
Jisu Kim <hellojs242@gmail.com>
2025-04-14 23:15:12 +0900

    Time: 75 ms (30.5%), Space: 98.6 MB (13.41%) - LeetHub

--- leetcode/0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ leetcode/0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -3,11 +3,12 @@ public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         // nums[i] == nums[j], abs(i - j) <= k를 만족하는 i, j
         int size = nums.size();
-        unordered_set<int> s;
+        unordered_map<int, int> m;
         for(int i = 0; i < size; ++i) {
-            if (i > k) s.erase(nums[i - k - 1]); // k index 이전을 삭제
-            if (s.count(nums[i])) return true;
-            s.insert(nums[i]);
+            if (m.count(nums[i]) && i - m[nums[i]] <= k) {
+                return true;
+            }
+            m[nums[i]] = i;
         }
         return false;
     }
