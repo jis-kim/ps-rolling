5f3891f9b28cdab5b55268f0b065b035709df09b
Jisu Kim <hellojs242@gmail.com>
2025-04-21 15:35:06 +0900

    Time: 11 ms (46.37%), Space: 18.9 MB (54.1%) - LeetHub

--- leetcode/0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.cpp
+++ leetcode/0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.cpp
@@ -17,24 +17,35 @@ public:
 */
 
 class Solution {
-private:
-    Node* nodes[6000] = {};
-
-    void preorder(Node *node, int level) {
-        if (!node) return;
-
-        if (nodes[level] != nullptr) { // 값이 있음
-            nodes[level]->next = node;
-        }
-        nodes[level] = node;
-        preorder(node->left, level + 1);
-        preorder(node->right, level + 1);
-    }
-
 public:
     Node* connect(Node* root) {
-        // 전위 순회하면서 이전 노드에 연결하는 식으로 하면 되지 않을까?
-        preorder(root, 0);
+        if (!root) return nullptr;
+
+        queue<Node*> q;
+        q.push(root);
+
+        while(!q.empty()) {
+            int size = q.size();
+            Node * prev = nullptr;
+
+            for (int i = 0; i < size; ++i) {
+                Node* node = q.front();
+                q.pop();
+
+                if (prev) {
+                    prev->next = node;
+                }
+                prev = node;
+
+                if (node->left) {
+                    q.push(node->left);
+                }
+                if (node->right) {
+                    q.push(node->right);
+                }
+            }
+        }
+
         return root;
     }
 };
