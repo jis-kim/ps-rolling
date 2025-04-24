c01e892a5a494992d973410470cf909fefcf95f2
Jisu Kim <hellojs242@gmail.com>
2025-04-24 23:48:33 +0900

    Time: 0 ms (100%), Space: 10.2 MB (98.72%) - LeetHub

--- leetcode/0139-word-break/0139-word-break-breakbyword.cpp
+++ leetcode/0139-word-break/0139-word-break-breakbyword.cpp
@@ -3,22 +3,22 @@ public:
     bool wordBreak(string s, vector<string>& wordDict) {
 
         bool dp[300] = {}; // false
-        unordered_set<string> st(wordDict.begin(), wordDict.end());
         dp[0] = true; 
 
         // 0 ~ i(i미포함)
         for (int i = 1; i <= s.size(); ++i) {
-            for (int j = 0; j < i; ++j) {
-                // i -> 0 ~ i까지 wordDict의 구성임. (i포함)
-                // 0 ~ j, j ~ i
-                // substr의 두번째 인자는 length
-                if (dp[j] && st.find(s.substr(j, i - j)) != st.end()) {
+            for (const string& w : wordDict) {
+                int start = i - w.length();
+                // 현재 단어가 start에 있다 가정.
+                // start가 0보다 크고, start이전이 true이고, start ~ w.lendth가 w와 같으면 true.
+                // 즉 1씩 증가시키는게 아니라 wordDict의 글자수로 쪼개서 본다.
+                // wordDict의 가능성을 탐색하는것임. 굉장히 똑똑한 방식인듯....
+                if (start >= 0 && dp[start] && s.substr(start, w.length()) == w) {
                     dp[i] = true;
                     break;
                 }
             }
         }
-
         return dp[s.size()];
     }
 };
