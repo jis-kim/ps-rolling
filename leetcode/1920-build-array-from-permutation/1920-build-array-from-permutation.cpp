d21b1a549ca5a6b6437bc6c02db45a3bbd828f05
Jisu Kim <hellojs242@gmail.com>
2025-05-06 23:39:12 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- leetcode/1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
+++ leetcode/1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
@@ -2,12 +2,10 @@ class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
         // 0 2 1 5 3 4
-        vector<int> ans(nums);
+        vector<int> ans(nums.length());
 
-        for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i] != i) {
-                ans[i] = nums[nums[i]];
-            }
+        for (int n : nums) {
+            ans.push_back(nums[n]);
         }
 
         return ans;
