310d1d01a9071311e85ff08ab6fae87f1789faa7
Jisu Kim <hellojs242@gmail.com>
2025-05-14 01:45:44 +0900

    Time: 19 ms (96.55%), Space: 32.4 MB (61.08%) - LeetHub

--- leetcode/3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.cpp
+++ leetcode/3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.cpp
@@ -3,15 +3,6 @@ private:
     const int mod = 1e9+7;
 public:
     int lengthAfterTransformations(string s, int t) {
-        // t - transformation to perform을 표현함.
-        // 매 s가 다음 룰에 따라 추가됨.
-        // 'z' -> "ab"
-        // z아니면 다음 알파벳으로 replace.
-        // 정확히 "t" 번 transformation 된 후의 길이를 리턴하시오.
-        //
-        // t -> max: 10^5
-        // a ~ z -> 26개
-        // t / 26 -> 한 사이클 도는 횟수임..
         // a가 z가 되고 ab가 될려면 26번의 transformation이 있어야 함.
         // b가 z가 되고 ab -> 25번
         // c -> 24번
@@ -19,29 +10,29 @@ public:
         // a -> 26회, b-> 25회다 이걸 어쩌몀ㄴ 좋지? 헷갈리구로...
         // 26 -> (26(2625)  25(2625))
 
-        int nums[26] = {};
-        for (char c: s) {
-            nums[c - 'a']++;
-        }
-        
-        // a~z가 t 번 순회 후 문자열 몇 개가 되는지 계산함.
-        for (int i = 0; i < t; ++i) {
-            int cur[26] = {};
-            for (int j = 0; j < 26; ++j) {
-                if (j == 25 && nums[j] > 0) { //z일때
-                    cur[0] = (cur[0] + nums[j]) % mod;
-                    cur[1] = (cur[1] + nums[j]) % mod;
-                } else {
-                    if (j < 25) cur[j + 1] = (cur[j + 1] +nums[j]) % mod;
-                }
+
+        // 순차적으로 적용되니까, b는 a에 t + 1번 순회한 결과와 똑같음.
+        // c는 t+2번, d는 t+3번..
+        // 그래서 z인 t + 25까지 하는거임!
+        // 'a'가 i번 변환됐을 때 최종적으로 몇의 길이가 되는가? 가 중요함.
+        vector<int> dp(t + 26); 
+
+        for (int i = 0; i < 26 + t; ++i) {
+            if (i < 26) {
+                dp[i] = 1; // 25회까지는 z임.
+            } else {
+                dp[i] = (dp[i - 25] + dp[i - 26]) % mod;
+                // i - 25 -> z가 아닌 문자열이 됨. (ab에서 b에 대응)
+                // i - 26 -> z가 됨. ("ab"에서 a에 대응)
+                // 이미 a, b가 파생된결과를 더하는거라서 26*2가 아님.
             }
-            for (int j = 0; j < 26; j++) nums[j] = cur[j];
         }
 
+
         int result = 0;
-        for (int n: nums) {
-            result = (result + n) % mod;
+        for (char c: s) {
+            result = (result + dp[t + c - 'a']) % mod;
         }
-        return result;
+        return result % mod;
     }
 };
